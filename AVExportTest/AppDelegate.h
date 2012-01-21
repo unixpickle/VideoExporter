a44c6449698fc4ed2bc499ba35f8bd28d3720163
Alex Nichol <alexnichol@comcast.net>
2012-01-20 20:48:15 -0500

    Initial Commit

---
+++ AVExportTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AVExportTest
+//
+//  Created by Alex Nichol on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
